71d7678b3a570a36b50e8762f3be486a9ba6931e
ZheikoN <negru.inger@gmail.com>
2022-05-27 11:17:00 +0000

    add function to recognise game and prepare first function to write rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log('this is working');
+function gameSelect() {
+
+    let standardGame = document.getElementById('standard-game');
+    let advancedGame = document.getElementById('advanced-game');
+
+    if (standardGame.checked == true) {
+        // alert(`selected ${standardGame.value}`);
+        standardGameRules();
+    } else if (advancedGame.checked == true) {
+        alert(`selected ${advancedGame.value}`);
+    } else {
+        alert(`nothing was selected`);
+    }
+
+}
+
+
+function standardGameRules() {
+    let standardGameRules = document.getElementById('rule');
+
+    standardGameRules.innerHTML += "<h2>standard rules</h2>";
+}
